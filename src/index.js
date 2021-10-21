d239e6779990727f9fe2ac33d55486a9be2f5ef1
tom tomsen <tom.tomsen@mailbox.org>
2021-10-21 13:08:26 +0200

    Add game canvas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('hello');
+const config = {
+    screen: {
+        width: 600,
+        height: 400,
+    },
+};
+
+const canvas = document.getElementById('game-screen');
+const ctx = canvas.getContext("2d");
+
+canvas.width = config.screen.width;
+canvas.height = config.screen.height;
+
+ctx.fillStyle = '#00ff99';
+ctx.fillRect(0, 0, config.screen.width, config.screen.height);
